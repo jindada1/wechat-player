945411dcd16e02b28814f0e7536211ac809359e3
jindada1 <zou_406143883@qq.com>
2021-11-19 00:07:43 +0800

    更新 api

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,6 +1,6 @@
 
 export const fetch = (params) => {
-  const baseUrl = "https://krishuang.top"
+  const baseUrl = "https://krishuang.top/relaxion"
   params.url = baseUrl + params.url;
   
   return new Promise((resolve, reject)=>{
